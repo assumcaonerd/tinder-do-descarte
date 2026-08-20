b5f8e11e963e90305676447a1d241acaada60f4a
assumcaonerd <assumcao@gmail.com>
2026-08-20 17:42:14 -0300

    feat: esqueleto do serviço de notificação

---
+++ descarte/notify.py
@@ -0,0 +1,6 @@
+from .models import Item
+
+
+def notify_coletor(coletor_id: str, item: Item, distancia: float) -> None:
+    # TODO: enviar notificação (por enquanto só print ou lista em memória)
+    pass
